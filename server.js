b3eda51fc49f4eafbde33ab45237b09f24b9e999
Paolo Conizzoli <paolo@conizzoli.com>
2016-12-01 23:14:32 +0100

    Access Control

--- server.js
+++ server.js
@@ -16,6 +16,24 @@ App.use(MethodOverride());
 App.use(BodyParser.json());
 App.use(BodyParser.urlencoded({extended: true}));
 
+App.use(function(req, res, next) {
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+})
+
 Db.initialisation();
 
 Http.createServer(App).listen(3000);
